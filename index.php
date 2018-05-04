9256411b3bb77e9a3b36db3f7bfc62d3592cee2b
plinioaraujo <plinioglad@gmail.com>
2018-05-04 07:49:57 -0300

    Estruturando o projeto Photomanager em PHP

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+require './lib/autoload.php';
+
+$smarty = new Smarty();
+
+$smarty -> setTemplateDir('view/');
+$smarty -> setCompileDir('view/compile');
+$smarty -> setCacheDir('view/cache');
+
+$smarty -> display('index.tpl');
+
+?>  
---
+++ view/compile/d581b498998e1bf2db5e2e605a9d17dc75de1fb4_0.file.index.tpl.php
@@ -0,0 +1,52 @@
+<?php
+/* Smarty version 3.1.32, created on 2018-05-04 12:43:59
+  from 'C:\xampp\htdocs\photomanager\view\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.32',
+  'unifunc' => 'content_5aec396f958994_65916264',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'd581b498998e1bf2db5e2e605a9d17dc75de1fb4' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\photomanager\\view\\index.tpl',
+      1 => 1525430468,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5aec396f958994_65916264 (Smarty_Internal_Template $_smarty_tpl) {
+?><!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <?php echo '<script'; ?>
+ src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+  </body>
+</html><?php }
+}
